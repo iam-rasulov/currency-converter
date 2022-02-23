7f98cde9f0fac5635244e1a182b25f2edfcd9ec3
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-02-23 18:04:17 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var elForm  = document.querySelector(".form");
+var elInput  = document.querySelector(".form__input");
+var elSelect  = document.querySelector(".form__select");
+var elResult  = document.querySelector(".form__result");
+
+
+
+
+
+
+elForm.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  var elInputVal = elInput.value;
+  var elSelectVal = elSelect.value;
+  if(elInputVal < 0){
+    (elResult.textContent = "nasiyag nmadr nmadr");
+  } else if (elInputVal >= 0){
+    elResult.textContent = (elInputVal * elSelectVal);
+  }
+  else{
+    console.log(elInput);
+  }
+
+});
+
